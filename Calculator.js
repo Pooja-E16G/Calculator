8d0fb98e0bc21d968e0aeabb58da0b8fa0bbe68f
Pooja-E16G <120215101+Pooja-E16G@users.noreply.github.com>
2022-12-28 20:14:24 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,34 @@
+let screen = document.getElementById('screen');
+ button= document.querySelectorAll("button");
+ let screenValue = "";
+ for(item of button){
+   
+item.addEventListener('click',(e)=>
+{
+    buttonText = e.target.innerText;
+    console.log("button text is", buttonText)
+
+    if(buttonText== 'X'){
+        buttonText= "*";
+        screenValue+= buttonText;
+        screen.value = screenValue;
+    
+    }
+    else if (buttonText== 'C'){
+        screenValue = "";
+        screen.value =  screenValue;
+    }
+    else if (buttonText== '='){
+        screen.value = eval(screenValue);
+    }
+    else{
+        screenValue+= buttonText;
+        screen.value = screenValue;
+    
+    }
+    
+
+})
+
+}
+
